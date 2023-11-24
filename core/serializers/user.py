294c5ad44a5b00d1e56830a74c32f893ae4737bb
Guilherme Tamanini <guilherme.gui.tama@gmail.com>
2023-11-24 16:35:12 -0300

    Mudenças na criação de senhas do usuario

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.hashers import make_password
 
 from core.models import User
 
@@ -7,3 +8,12 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+    def validate_password(self, value: str) -> str:
+        """
+        Hash value passed by user.
+
+        :param value: password of a user
+        :return: a hashed version of the password
+        """
+        return make_password(value)
