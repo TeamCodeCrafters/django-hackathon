30ce726e309f3c5de5ceb0a3af494c0845d64d49
Ana <analuizadiasdarocha212@gmail.com>
2023-11-25 02:51:22 -0300

    Alteração nas equipes

--- projeto/models/equipe.py
+++ projeto/models/equipe.py
@@ -9,11 +9,7 @@ class Equipe(models.Model):
     integrante5 = models.CharField(max_length=100, blank=True)
     integrante6 = models.CharField(max_length=100, blank=True)
     edicao = models.CharField(max_length=4, default=2023)
-   
 
-    @property
-    def ano_criacao(self):
-        return self.data.year
     
     def __str__(self):
         return f"{self.nome} - {self.ano_criacao}"
