ec0017f5d03e696f2c546293dc3a76922779c3eb
luizatorrens <luiza.torrenss@gmail.com>
2023-11-25 08:12:03 -0300

    Authenticated da AvaliaçãoSerialiser canvelado

--- projeto/views/avaliacao.py
+++ projeto/views/avaliacao.py
@@ -1,12 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from ..models import Avaliacao
 from ..serializers import AvaliacaoSerializer
-from rest_framework.permissions import IsAuthenticated
+
 
 
 class AvaliacaoViewSet(ModelViewSet):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
-    permission_classes = [
-        IsAuthenticated,
-    ]
