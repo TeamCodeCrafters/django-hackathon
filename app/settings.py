47f28a2ceb690a5e9b9339a1bf817d4a891a69e1
Ana <aninha10.desouza@gmail.com>
2023-11-25 04:49:11 -0300

    page_size

--- app/settings.py
+++ app/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
     # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.DjangoModelPermissions",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 1000,
 }
 
 SPECTACULAR_SETTINGS = {
